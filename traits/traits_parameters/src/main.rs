c65d1e969a950c96a363a196b8ef7a72e50a4dae
noelcv <noelguiavieira@gmail.com>
2023-02-01 21:32:22 +0100

    multiple trait parameters and plus syntax

--- traits/traits_parameters/src/main.rs
+++ traits/traits_parameters/src/main.rs
@@ -1,19 +1,29 @@
+use std::fmt::{Display, Formatter, Result};
+
 pub trait Summary {
     fn summarize(&self) -> String;
 }
 
 #[derive(Debug)]
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-    pub source: String,
-}
+    pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+        pub source: String,
+    }
+    
+    impl Summary for NewsArticle {
+        fn summarize(&self) -> String {
+            format!("{}, by {}({})", self.headline, self.author, self.source)
+        }
+    }
+    
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {}({})", self.headline, self.author, self.source)
+
+impl Display for NewsArticle {
+    fn fmt (&self, f: &mut Formatter) -> Result {
+        write!(f, "{}, by {}  ({})", self.headline, self.author, self.source)
     }
 }
 
@@ -31,19 +41,27 @@ pub fn notify_verbose<T: Summary>(item: &T) {
 }
 
 //While here the `impl trait` syntax allows for any type that implements the Summary trait to be passed as an argument to the function...
-pub fn notify_multiple(item1: &impl Summary, item2: &impl Summary) {
-    println!("[NOTIFY MULTIPLE: BREAKING NEWS 1], {}", item1.summarize());
-    println!("[NOTIFY MULTIPLE: BREAKING NEWS 2], {}", item2.summarize());
+pub fn notify_multiple_parameters(item1: &impl Summary, item2: &impl Summary) {
+    println!("[NOTIFY MULTIPLE PARAMETERS: BREAKING NEWS 1], {}", item1.summarize());
+    println!("[NOTIFY MULTIPLE PARAMETERS: BREAKING NEWS 2], {}", item2.summarize());
 }
 
 
 //...here the generic type T used trait bound syntax requires / constrains that both arguments are of the same type and implement the Summary trait
-pub fn notify_multiple_strict<T: Summary> (item1: &T, item2: &T) {
-    println!("[NOTIFY MULTIPLE STRICT: BREAKING NEWS 1], {}", item1.summarize());
-    println!("[NOTIFY MULTIPLE STRICT: BREAKING NEWS 2], {}", item2.summarize());
+pub fn notify_multiple_parameters_strict<T: Summary> (item1: &T, item2: &T) {
+    println!("[NOTIFY MULTIPLE PARAMETERS STRICT: BREAKING NEWS 1], {}", item1.summarize());
+    println!("[NOTIFY MULTIPLE PARAMETERS STRICT: BREAKING NEWS 2], {}", item2.summarize());
 }
 
+//WITH MULTIPLE TRAITS
+pub fn notify_with_several_traits(item: &(impl Summary + Display)) {
+    println!("[NOTIFY WITH SEVERAL TRAITS: BREAKING NEWS], {}", item.summarize());
+}
 
+//WITH MULTIPLE TRAITS AND `+` SYNTAX
+pub fn notify_with_plus_syntax<T: Summary + Display>(item: &T) {
+    println!("[NOTIFY WITH + SYNTAX] {}", item.summarize());
+}
 
 pub struct Tweet {
     pub username: String,
@@ -93,6 +111,9 @@ fn main() {
     notify(&tweet_one);
     notify_verbose(&tweet_two);
     
-    notify_multiple(&article_one, &tweet_one);
-    notify_multiple_strict(&article_one, &article_one);
+    notify_multiple_parameters(&article_one, &tweet_one);
+    notify_multiple_parameters_strict(&article_one, &article_one);
+    
+    notify_with_several_traits(&article_one);
+    notify_with_plus_syntax(&article_one);
 }
