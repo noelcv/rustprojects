30166074420379cf1a00b09ed313d2dbef2ad09d
noelcv <noelguiavieira@gmail.com>
2023-02-17 20:40:00 +0100

    minigrep: reading a file

--- minigrep/README.md
+++ minigrep/README.md
@@ -18,3 +18,24 @@
 Searching for needle
 In file haystack
 ```
+
+We added the `poem.txt` at the root of the project,
+
+```bash
+➜  minigrep git:(main) ✗ cargo run -- the poem.txt
+   Compiling minigrep v0.1.0 (/Users/noel/rustprojects/minigrep)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.58s
+     Running `target/debug/minigrep the poem.txt`
+Searching for the
+In file poem.txt
+With text: 
+I'm nobody! Who are you?
+Are you nobody, too?
+Then there's a pair of us - don't tell!
+They'd banish us, you know.
+
+How dreary to be somebody!
+How public, like a frog
+To tell your name the livelong day
+To an admiring bog!
+```
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,18 @@
 use std::env; //bring it to scope so we can use args()
+use std::fs; //to handle file system operations
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); // turn the iterator into a collection of values, in this case a Vector of strings
+    let args: Vec<String> = env::args().collect(); //turn the iterator into a collection of values, in this case a Vector of strings
     // dbg!(args);
     
     let query = &args[1];
     let file_path = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file"); //returns a std::io::Result<String>
+
+    println!("With text: \n{contents}");
 }
 
